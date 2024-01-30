722cb86d21a9f6a32f5283b7037797bf89d38d44
cesr19augusto <augustocesar875@gmail.com>
2024-01-30 17:03:01 -0300

    add da troca de imagens via js

--- script.js
+++ script.js
@@ -17,3 +17,40 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+var imagens = [
+    "./imagens_jpeg/TF_BAGEACADEMY.jpeg",
+    "./imagens_jpeg/TF_ERP.jpeg",
+    "./imagens_png/TF_Shop.png"
+  ];
+  
+  var indiceAtual = 0;
+  var intervaloTroca = 7000; // Intervalo de troca em milissegundos (3 segundos)
+  
+  function trocarImagemAutomaticamente() {
+    document.getElementById("imagem").src = imagens[indiceAtual];
+    indiceAtual = (indiceAtual + 1) % imagens.length; // Avança para a próxima imagem
+  }
+  
+  // Iniciar a troca automática de imagens
+  var intervaloID = setInterval(trocarImagemAutomaticamente, intervaloTroca);
+  
+  // Se você quiser parar a troca automática após um certo tempo, descomente a linha abaixo
+  // setTimeout(function() { clearInterval(intervaloID); }, 15000); // Isso irá parar a troca automática após 15 segundos
+  
+
+
+
+/*
+var imagem1 = "TF_BAGEACADEMY.jpeg"; 
+var imagem2 = "TF_ERP.jpeg";
+var imagem3 = "TF_Shop.png";
+
+function trocar (){
+    document.getElementById("imagem").src = imagem1;
+    let aux = imagem1;
+    imagem1 = imagem2;
+    imagem2 = aux;
+}
+
+<button onclick="trocar()">Trocar imagem</button> */
